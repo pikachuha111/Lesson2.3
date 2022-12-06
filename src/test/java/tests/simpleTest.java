052e2d2ffbefb1145e26158099d16f229803a726
Nikita_Solovev <nickdanse99@gmail.com>
2022-12-06 17:02:31 +0300

    initial

---
+++ src/test/java/tests/simpleTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class simpleTest extends BaseTest {
+
+    @Test
+    public void test1() {
+        System.out.println("тест");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("тест номер 2");
+    }
+}

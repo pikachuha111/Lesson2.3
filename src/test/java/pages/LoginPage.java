32ee29d5ae949415976d727e3a9c7c6b38ecd14b
Nikita_Solovev <nickdanse99@gmail.com>
2022-12-13 21:42:16 +0300

    pageObject

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import baseEntities.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasePage {
+
+    //Блок описания локаторов для элементов
+    private final By emailInputLocator = By.id("name");
+    private final By passwordInputLocator = By.id("password");
+    private final By logInButtonLocator = By.id("button_primary");
+    private final By errorTextLocator = By.className("error-text");
+
+    //Блок инициализации страницы
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected By getPageIdentifier() {
+        return logInButtonLocator;
+    }
+
+    //Блок атомарных методов
+
+    public WebElement getEmailInput() { return driver.findElement(emailInputLocator);}
+    public WebElement getPasswordInput() { return driver.findElement(passwordInputLocator);}
+    public WebElement getLogInButton() { return driver.findElement(logInButtonLocator);}
+    public WebElement getErrorTextElement() { return driver.findElement(errorTextLocator);}
+
+
+}
